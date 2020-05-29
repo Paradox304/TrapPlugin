275f67c41f67e1f277043189a1b4f6e621ad1700
Paradox <maazkashif492@outlook.com>
2020-05-29 22:51:05 +0500

    Plugin ready for use.

--- TrapPlugin/TrapPlugin/TrapPlugin.cs
+++ TrapPlugin/TrapPlugin/TrapPlugin.cs
@@ -28,46 +28,6 @@ namespace TrapPlugin
         {
             Rocket.Core.Logging.Logger.Log("Reset Plugin has been unloaded!");
         }
-
-        /*
-        private void BarricadeSpawned(BarricadeRegion region, BarricadeDrop drop)
-        {
-            var interactable = drop.interactable;
-            if (interactable is InteractableTrap)
-            {
-                Storage.barricadeDrops.Add(drop);
-                
-            }
-        }
-
-        private void SalvageBarricade(CSteamID steamID, byte x, byte y, ushort plant, ushort index, ref bool shouldAllow)
-        {
-            if (Storage.barricadeDrops.Count == 0) return;
-
-            var drop = Storage.barricadeDrops.FirstOrDefault(k => BarricadeManager.regions[x, y].drops[index].model == k.model);
-
-            if (drop == null) return;
-
-            Storage.barricadeDrops.Remove(drop);
-        }
-
-        
-        private void DamageBarricade(CSteamID instigatorSteamID, Transform barricadeTransform, ref ushort pendingTotalDamage, ref bool shouldAllow, EDamageOrigin damageOrigin)
-        {
-            if (!BarricadeManager.tryGetInfo(barricadeTransform, out byte x, out byte y, out ushort plant, out ushort index, out BarricadeRegion region)) return;
-            if (BarricadeManager.regions[x, y].barricades[index].barricade.health < pendingTotalDamage)
-            {
-                if (Storage.barricadeDrops.Count == 0) return;
-
-                var drop = Storage.barricadeDrops.FirstOrDefault(k => BarricadeManager.regions[x, y].drops[index].model == k.model);
-
-                if (drop == null) return;
-
-                Storage.barricadeDrops.Remove(drop);
-            }
-
-        } 
-        */
     }
 
     [HarmonyPatch(typeof(InteractableTrap), "OnTriggerEnter")]
@@ -76,28 +36,17 @@ namespace TrapPlugin
         [HarmonyPrefix]
         public static bool Prefix(Collider other, InteractableTrap __instance)
         {
-            Rocket.Core.Logging.Logger.Log("1");
             if (!other.transform.CompareTag("Player")) return true; // Not a player, we dont care, it can activate.
-            Rocket.Core.Logging.Logger.Log("2");
             if (!Provider.isPvP || other.transform.parent.CompareTag("Vehicle")) return false; // PvP is disabled or the player is in a vehicle, so ignore.
-            Rocket.Core.Logging.Logger.Log("3");
             var player = DamageTool.getPlayer(other.transform);
             if (player == null) return true; // Player not found, something went horribly wrong in nelson code
-            Rocket.Core.Logging.Logger.Log("4");
-            BarricadeManager.tryGetInfo(__instance.transform, out byte _, out byte _, out ushort _, out ushort index, out BarricadeRegion region);
-            Rocket.Core.Logging.Logger.Log("5");
+            if (!BarricadeManager.tryGetInfo(__instance.transform.parent, out byte x, out byte y, out ushort plant, out ushort index, out BarricadeRegion region)) return true;
             var barricadeData = region.barricades[index];
-            Rocket.Core.Logging.Logger.Log("6");
+            if (barricadeData.owner == player.channel.owner.playerID.steamID.m_SteamID) return false;
             if (barricadeData.group == player.quests.groupID.m_SteamID) return false;
-            Rocket.Core.Logging.Logger.Log("7");
             return true;
         }
     }
-
-    /* public static class Storage
-    {
-        public static List<BarricadeDrop> barricadeDrops = new List<BarricadeDrop>();
-    } */
 }
 
 
